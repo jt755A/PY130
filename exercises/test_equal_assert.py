5a3ba6d7259f18e893bc66ff6e214c957f6a11aa
jt755A <jot755A@gmail.com>
2025-08-19 17:49:59 -0700

    more py130 progress

---
+++ exercises/test_equal_assert.py
@@ -0,0 +1,2 @@
+self.assertTrue(value.lower == 'xyz', 'string is not xyz')
+self.assertEqual('xyz', value.lower())
---
+++ exercises/testing_bool.py
@@ -0,0 +1,8 @@
+import unittest
+
+# class NumTest(unittest.TestClass):
+
+    self.assertTrue(value % 2 != 0, 'value is not odd')
+
+# if __name__ == '__main__':
+#     unittest.main()
